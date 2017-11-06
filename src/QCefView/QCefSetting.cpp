b6daa5c094befeafbb02e34d855ef8b88b541f91
Ben Hetherington <ben.hetherington@hawkeyeinnovations.com>
2017-11-06 11:57:33 +0000

    Allow QCefSetting to be set before CEF is initialised
    Previously, the call to CCefSetting::initializeInstance() when initialising CEF would overwrite any settings set beforehand through QCefSetting.

--- src/QCefView/QCefSetting.cpp
+++ src/QCefView/QCefSetting.cpp
@@ -4,44 +4,52 @@
 
 void QCefSetting::setBrowserSubProcessPath(const QString& path)
 {
+	CCefSetting::initializeInstance();
 	CCefSetting::browser_sub_process_path.FromString(path.toStdString());
 }
 
 const QString QCefSetting::browserSubProcessPath()
 {
+	CCefSetting::initializeInstance();
 	return QString::fromStdString(
 		CCefSetting::browser_sub_process_path.ToString());
 }
 
 void QCefSetting::setResourceDirectoryPath(const QString& path)
 {
+	CCefSetting::initializeInstance();
 	CCefSetting::resource_directory_path.FromString(path.toStdString());
 }
 
 const QString QCefSetting::resourceDirectoryPath()
 {
+	CCefSetting::initializeInstance();
 	return QString::fromStdString(
 		CCefSetting::resource_directory_path.ToString());
 }
 
 void QCefSetting::setLocalesDirectoryPath(const QString& path)
 {
+	CCefSetting::initializeInstance();
 	CCefSetting::locales_directory_path.FromString(path.toStdString());
 }
 
 const QString QCefSetting::localesDirectoryPath()
 {
+	CCefSetting::initializeInstance();
 	return QString::fromStdString(
 		CCefSetting::locales_directory_path.ToString());
 }
 
 void QCefSetting::setUserAgent(const QString& agent)
 {
+	CCefSetting::initializeInstance();
 	CCefSetting::user_agent.FromString(agent.toStdString());
 }
 
 const QString QCefSetting::userAgent()
 {
+	CCefSetting::initializeInstance();
 	return QString::fromStdString(
 		CCefSetting::user_agent.ToString());
 }
