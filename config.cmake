ced6a45da698adf56ff5c70911b3a62e3ed9d212
Sheen <sheentianshen@gmail.com>
2021-04-01 17:53:34 +0800

    Download CEF binary package and extract it automatically

--- config.cmake
+++ config.cmake
@@ -2,6 +2,7 @@
 # Build environment configuration file for QCefView
 #
 
+#################################################################################
 #
 # The Qt SDK path
 #
@@ -11,20 +12,47 @@ set(QT_SDK_DIR
 )
 
 #
-# The root dir of the CEF SDK
+# The link for downloading the CEF binary sdk 
 #
-set(CEF_SDK_DIR
-  # Change this value to the CEF binary distribution path of your build environment
-  "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_89.0.12+g2b76680+chromium-89.0.4389.90_windows64"
+set(CEF_SDK_URL
+  # Change this value to swith between different CEF versions.
+  "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_windows64.tar.bz2" 
 )
 
-#################################################################################
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Usually, there is NO NEED to modify the following config
 #
-# For CI system
+# Download CEF binary package
 #
-if (DEFINED ENV{APPVEYOR})
-  set(QT_SDK_DIR
-    # Change this value to the Qt SDK path of your build environment
-    "$ENV{QTDIR}"
-  )
+set (CEF_SDK_PACKAGE "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_package.tar.bz2")
+if(NOT EXISTS "${CEF_SDK_PACKAGE}")
+  file(DOWNLOAD 
+    "${CEF_SDK_URL}"        # URL 
+    "${CEF_SDK_PACKAGE}"    # Local Path
+    SHOW_PROGRESS 
+    TLS_VERIFY ON)
 endif()
+
+#
+# Extact the CEF SDK dir
+#
+file(GLOB CEF_SDK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_*")
+
+#
+# Extract CEF binary package
+#
+if (NOT EXISTS ${CEF_SDK_DIR})
+  message(STATUS "CEF SDK dir does not exist, extracting new one....")
+
+  # Extract
+  file(ARCHIVE_EXTRACT
+    INPUT "${CEF_SDK_PACKAGE}"
+    DESTINATION "${CMAKE_CURRENT_SOURCE_DIR}/dep")
+
+  # Capture the dir name
+  file(GLOB CEF_SDK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_*")
+endif()
+
+# output
+message(STATUS "CEF SDK dir: ${CEF_SDK_DIR}")
